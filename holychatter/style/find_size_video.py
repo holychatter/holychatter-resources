f91421ed33175971174a10bfe73ccbd42dc96373
Jocelyn Martin <jmartin@aldebaran-robotics.com>
2020-12-02 12:10:24 +0100

    Add Playlist-Script font family and improve hc-width-content-with-refs

---
+++ holychatter/style/find_size_video.py
@@ -0,0 +1,25 @@
+
+f= open("video_size.css","w")
+
+winowSize=1951
+step=25
+
+while True:
+  videoSize = 0
+  if winowSize > 1100:
+    videoSize = winowSize - 601
+  else:
+    videoSize = winowSize - 51
+  if winowSize == 1951:
+    f.write("@media screen and (min-width: " + str(winowSize) + "px) {\n")
+  elif winowSize == 301:
+    f.write("@media only screen and (max-width: 350px) {\n")
+  else:
+    f.write("@media screen and (min-width: " + str(winowSize) + "px)  and (max-width: " + str(winowSize+step-1) + "px) {\n")
+  f.write(".hc-width-content-with-refs { width: " + str(videoSize) + "px; }\n")
+  f.write("}\n\n")
+  if winowSize == 301:
+    break
+  winowSize-=step
+
+f.close()
