c1afc1f44b9c9756b64b762a1ba9422b76930355
Jocelyn Martin <jmartin@aldebaran-robotics.com>
2020-12-18 00:13:32 +0100

    Modify chat window height

--- holychatter/style/find_max_height_dialog.py
+++ holychatter/style/find_max_height_dialog.py
@@ -1,21 +1,21 @@
 
 f= open("dialog_max_height.css","w")
 
-winowHeight=1551
+windowHeight=1551
 step=25
 
 while True:
-  height = winowHeight - 150
-  if winowHeight == 1551:
-    f.write("@media screen and (min-height: " + str(winowHeight) + "px) {\n")
-  elif winowHeight == 301:
-    f.write("@media only screen and (max-height: " + str(winowHeight+step-1) + "px) {\n")
+  height = windowHeight - 220
+  if windowHeight == 1551:
+    f.write("@media screen and (min-height: " + str(windowHeight) + "px) {\n")
+  elif windowHeight == 301:
+    f.write("@media only screen and (max-height: " + str(windowHeight+step-1) + "px) {\n")
   else:
-    f.write("@media screen and (min-height: " + str(winowHeight) + "px)  and (max-height: " + str(winowHeight+step-1) + "px) {\n")
+    f.write("@media screen and (min-height: " + str(windowHeight) + "px)  and (max-height: " + str(windowHeight+step-1) + "px) {\n")
   f.write(".hc-dialog-height-match-screen { height: " + str(height) + "px; }\n")
   f.write("}\n\n")
-  if winowHeight == 301:
+  if windowHeight == 301:
     break
-  winowHeight-=step
+  windowHeight-=step
 
 f.close()
