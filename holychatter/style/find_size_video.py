2a2dc6a4a54f30d9289df9bb3e6b30ab6968bc93
Jocelyn Martin <jmartin@aldebaran-robotics.com>
2020-12-25 11:42:37 +0100

    Remove hc-width-image-with-refs

--- holychatter/style/find_size_video.py
+++ holychatter/style/find_size_video.py
@@ -1,29 +1,25 @@
 
 f= open("video_size-1.css","w")
 
-winowSize=1951
+windowSize=1951
 step=25
 
 while True:
   videoSize = 0
-  if winowSize > 1100:
-    videoSize = winowSize - 601
+  if windowSize > 1100:
+    videoSize = windowSize - 601
   else:
-    videoSize = winowSize - 51
-  if winowSize == 1951:
-    f.write("@media screen and (min-width: " + str(winowSize) + "px) {\n")
-  elif winowSize == 301:
-    f.write("@media only screen and (max-width: " + str(winowSize+step-1) + "px) {\n")
+    videoSize = windowSize - 51
+  if windowSize == 1951:
+    f.write("@media screen and (min-width: " + str(windowSize) + "px) {\n")
+  elif windowSize == 301:
+    f.write("@media only screen and (max-width: " + str(windowSize+step-1) + "px) {\n")
   else:
-    f.write("@media screen and (min-width: " + str(winowSize) + "px)  and (max-width: " + str(winowSize+step-1) + "px) {\n")
+    f.write("@media screen and (min-width: " + str(windowSize) + "px)  and (max-width: " + str(windowSize+step-1) + "px) {\n")
   f.write(".hc-width-content-with-refs { width: " + str(videoSize) + "px; }\n")
-  if videoSize > 500:
-    f.write(".hc-width-image-with-refs { width: 500px; }\n")
-  else:
-    f.write(".hc-width-image-with-refs { width: " + str(videoSize) + "px; }\n")
   f.write("}\n\n")
-  if winowSize == 301:
+  if windowSize == 301:
     break
-  winowSize-=step
+  windowSize-=step
 
 f.close()
